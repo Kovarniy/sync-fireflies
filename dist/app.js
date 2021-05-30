7da6c62a19f2f6b6770a8195fc4493a2c68bb36b
Борислав Ландик <borislavlandik00@gmail.com>
2021-05-30 18:42:36 +0800

    chore(page-settings): remove app.js from repo

--- dist/app.js
+++
@@ -1,5 +0,0 @@
-import { FirefliesGenerator } from './services/fireflies-generator.js';
-import { Canvas } from './services/fireflies-renderer.js';
-const canvas = new Canvas('canvas');
-const fireflies = FirefliesGenerator.generate(canvas.width, canvas.height);
-canvas.render(fireflies);
