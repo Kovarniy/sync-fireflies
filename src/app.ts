96d65e03cb7d6a423682ce0a4f93f236ab732e0a
kovarniy <kovarniy12@yandex.ru>
2021-06-01 21:10:33 +0800

    feat(app): add deploy script

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import { Firefly } from './model/firefly.js';
 import { FirefliesGenerator } from './services/fireflies-generator.js';
 import { Canvas } from './services/fireflies-renderer.js';
 import { PageSettings } from './services/page-settings.js';
+import run from '@jamesives/github-pages-deploy-action';
 
 export function regenerateCanvas(canvas: Canvas): void {
   canvas.stop();
@@ -34,7 +35,7 @@ document.addEventListener('DOMContentLoaded', () => {
     globalSpeed: {
       baseSelector: 'speed-counter-range',
       counterSelector: 'speed-counter-current',
-    }
+    },
   }).init();
 
   const fireflies: Firefly[] = FirefliesGenerator.generate(
@@ -44,7 +45,19 @@ document.addEventListener('DOMContentLoaded', () => {
 
   canvas.render(fireflies);
 
-  document.getElementById('regenerate-button')?.addEventListener('click', () => {
-    regenerateCanvas(canvas);
-  });
+  document
+    .getElementById('regenerate-button')
+    ?.addEventListener('click', () => {
+      regenerateCanvas(canvas);
+    });
+});
+
+run({
+  // token: process.env['firefly-token'],
+  branch: 'gh-pages',
+  folder: 'dist',
+  repositoryName: 'kovarniy/sync-fireflies',
+  silent: true,
+  workspace: 'src',
+  isTest: 0,
 });
